46f27bc4cbce483a3a92c65fe895a344f4ff58eb
Jacob Schnoor <schno059@d.umn.edu>
2021-05-06 22:50:52 -0500

    testing

--- Roms/__init__.py
+++ Roms/__init__.py
@@ -1,2 +1,9 @@
-for x in range(4,40,2):
-  print(x)
+import wikipedia
+import re
+
+regex = "(.*\\bnintendo\\b\\s.*)|" \
+        "(.*\\bplaystation\\b\\s.*)"
+page = wikipedia.page("Home video game console")
+consoles = [s for s in page.links if re.match(regex, s, re.IGNORECASE)]
+for x in consoles:
+    print(x)
