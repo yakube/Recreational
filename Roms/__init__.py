620d54d31a566a308326e39d6e69079e91e22803
Yakube <44039980+yakube@users.noreply.github.com>
2021-06-13 19:11:59 -0500

    Delete Roms directory

--- Roms/__init__.py
+++
@@ -1,9 +0,0 @@
-import wikipedia
-import re
-
-regex = "(.*\\bnintendo\\b\\s.*)|" \
-        "(.*\\bplaystation\\b\\s.*)"
-page = wikipedia.page("Home video game console")
-consoles = [s for s in page.links if re.match(regex, s, re.IGNORECASE)]
-for x in consoles:
-    print(x)
